95ad6b5a44820b92751f58f083b381c10b1cddcd
Fynn Feldpausch <fynn.feldpausch@mindsmash.com>
2015-07-20 12:37:19 +0200

    fixed template paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
                 expand: true,
                 cwd: 'src',
                 src: ['*.js'],
-                dest: 'build'
+                dest: 'dist'
             }
         },
         
